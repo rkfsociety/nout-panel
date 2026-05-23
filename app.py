85d09e9d78311b6370a8112f807359b6e32ccb27
rkfsociety <romankuzminvital@gmail.com>
2026-05-23 21:39:07 +0200

    Вкладки навигации сверху вместо ссылок внизу страницы.
    Общие nav.css/nav.js на мониторинге, чате и управлении.

--- app.py
+++ app.py
@@ -222,6 +222,8 @@ class PanelHandler(BaseHTTPRequestHandler):
             "/chart.umd.min.js": ("chart.umd.min.js", "application/javascript; charset=utf-8"),
             "/xterm.min.js": ("xterm.min.js", "application/javascript; charset=utf-8"),
             "/xterm.min.css": ("xterm.min.css", "text/css; charset=utf-8"),
+            "/nav.css": ("nav.css", "text/css; charset=utf-8"),
+            "/nav.js": ("nav.js", "application/javascript; charset=utf-8"),
         }
         if path in static_map:
             name, ctype = static_map[path]
